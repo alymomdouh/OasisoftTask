42064af6698c16694bd5936b6719e151039ad714
Aly Mamdouh <aly.mamdouh@interactts.com>
2024-09-11 09:34:16 +0300

    add ToDoProfile

---
+++ Applications/Profiles/ToDoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using OasisoftTask.Applications.Dtos.ToDoDtos;
+using OasisoftTask.Core.DomainModels;
+
+namespace OasisoftTask.Applications.Profiles
+{
+    public class ToDoProfile : Profile
+    {
+        protected ToDoProfile()
+        {
+            CreateMap<CreateToDo, ToDo>();
+            CreateMap<ToDo, ListToDo>();
+        }
+    }
+}
