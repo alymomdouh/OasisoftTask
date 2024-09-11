c7b87f801d732a07d27c67727c807436d0bd2e9c
Aly Mamdouh <aly.mamdouh@interactts.com>
2024-09-11 09:32:25 +0300

    add ApplicationDbContext   and migrations

---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OasisoftTask.Core.DomainModels;
+using System.Reflection;
+
+namespace OasisoftTask.Infrastructure
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
+    {
+        protected readonly IConfiguration _configuration;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+            options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+        public DbSet<ToDo> ToDos { get; set; }
+    }
+}
