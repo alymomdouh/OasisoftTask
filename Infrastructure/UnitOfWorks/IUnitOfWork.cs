d4803f78ccf80226790305158243e4b0da105ae2
Aly Mamdouh <aly.mamdouh@interactts.com>
2024-09-11 09:31:50 +0300

    add IUnitOfWork

---
+++ Infrastructure/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using OasisoftTask.Core.DomainModels;
+using OasisoftTask.Infrastructure.IRepositories;
+
+namespace OasisoftTask.Infrastructure.UnitOfWorks
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<T> Repository<T>() where T : BaseEntity;
+        Task<int> Save(CancellationToken cancellationToken = default);
+        Task Rollback();
+    }
+}
---
+++ Infrastructure/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,69 @@
+﻿using OasisoftTask.Core.DomainModels;
+using OasisoftTask.Infrastructure.IRepositories;
+using OasisoftTask.Infrastructure.Repositories;
+using System.Collections;
+
+namespace OasisoftTask.Infrastructure.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private Hashtable _repositories;
+        private bool disposed;
+
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public IGenericRepository<T> Repository<T>() where T : BaseEntity
+        {
+            if (_repositories == null)
+                _repositories = new Hashtable();
+
+            var type = typeof(T).Name;
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(GenericRepository<>);
+
+                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(T)), _dbContext);
+
+                _repositories.Add(type, repositoryInstance);
+            }
+
+            return (IGenericRepository<T>)_repositories[type];
+        }
+
+        public Task Rollback()
+        {
+            _dbContext.ChangeTracker.Entries().ToList().ForEach(x => x.Reload());
+            return Task.CompletedTask;
+        }
+
+        public async Task<int> Save(CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                if (disposing)
+                {
+                    //dispose managed resources
+                    _dbContext.Dispose();
+                }
+            }
+            //dispose unmanaged resources
+            disposed = true;
+        }
+    }
+}
